add9b21ed894a7a6e88525895979b70516e1272e
Tobias Hieta <tobias@hieta.se>
2012-04-13 10:42:38 +0000

    Minor bug correcting in redhat script

--- redhat/create_plex_pkg.sh
+++ redhat/create_plex_pkg.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 if [[ $NODE_NAME == Linux-Ubuntu-Lucid-* ]]; then
+    echo "build RPM"
+    rm -rf redhat_temp
 	mkdir redhat_temp
 	cd redhat_temp
+    mkdir -p usr/lib/plexmediaserver
 	cp -r ../files/etc ../files/lib ../files/plexmediaserver.spec ../files/usr .
-	cp -r ${PLX_SRC}/* usr/lib/plexmediaserver/.
+	cp -r ${PLX_SRCDIR}/* usr/lib/plexmediaserver/.
 	find ./usr/lib/plexmediaserver/ -type d | cut -d. -f2- | sed 's/\/usr/%dir "\/usr/g' | sed 's/$/"/' >> plexmediaserver.spec
 	find ./usr/lib/plexmediaserver/ -type f | cut -d. -f2- | sed 's/$/"/' | sed 's/\/usr/"\/usr/g' >> plexmediaserver.spec
 	dir=`pwd`
@@ -12,6 +15,6 @@ if [[ $NODE_NAME == Linux-Ubuntu-Lucid-* ]]; then
 	rm -f plexmediaserver.spec
 	cd ..
 	rpmbuild -bb --buildroot=${dir} --quiet plexmediaserver-${PLX_VERSION}.spec
-	mv *.rpm out/.
+	mv *.rpm ${PLX_OUTDIR} 
 	rm -f *.spec
 fi
