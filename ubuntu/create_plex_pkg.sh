e9f84108fcb8719ea29ce25a1c53178c2d559a52
Tobias Hieta <tobias@hieta.se>
2012-05-02 13:48:04 +0200

    Seems like we have to create the zip file before we build debian packages

--- ubuntu/create_plex_pkg.sh
+++ ubuntu/create_plex_pkg.sh
@@ -4,18 +4,19 @@ if [[ $NODE_NAME == Linux-Ubuntu* ]];
 then
 	echo "Building Ubuntu package"
 	rm -f *.deb *.changes
-	mkdir debian-tmp
-	cd debian-tmp
+	mkdir PlexMediaServer-$PLX_VERSION-USC
+	cd PlexMediaServer-$PLX_VERSION-USC
     cp ../plexmediamanager.* .
 	ln -s $PLX_SRCDIR src
 	cp -r ../debian .
 	export EMAIL="jenkins@plexapp.com"
 	export NAME="Plex CI Team"
 	dch -b -v $PLX_VERSION "Automatic build $BUILD_NUMBER"
+    cd ..
+    zip -r PlexMediaServer-$PLX_VERSION-`uname -m`-USC.zip PlexMediaServer-$PLX_VERSION-USC
+    cd PlexMediaServer-$PLX_VERSION-USC
 	fakeroot dpkg-buildpackage -us -uc -b
 	cd ..
-    mv debian-tmp PlexMediaServer-$PLX_VERSION-USC
-    zip -r PlexMediaServer-$PLX_VERSION-`uname -m`-USC.zip PlexMediaServer-$PLX_VERSION-USC
 	mv *.deb *.changes *.zip $PLX_OUTDIR
 	rm -rf PlexMediaServer-$PLX_VERSION-USC
 fi
