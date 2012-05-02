ea47f2c661ce0c04523b28dd3e11067c1180ab0a
Tobias Hieta <tobias@hieta.se>
2012-05-02 13:27:13 +0200

    Make Ubuntu Software Center package as well

--- ubuntu/create_plex_pkg.sh
+++ ubuntu/create_plex_pkg.sh
@@ -11,10 +11,12 @@ then
 	cp -r ../debian .
 	export EMAIL="jenkins@plexapp.com"
 	export NAME="Plex CI Team"
-	dch -b -v $PLX_VERSION "Hudson build $BUILD_NUMBER"
+	dch -b -v $PLX_VERSION "Automatic build $BUILD_NUMBER"
 	fakeroot dpkg-buildpackage -us -uc -b
 	cd ..
-	mv *.deb *.changes $PLX_OUTDIR
+    mv debian-tmp PlexMediaServer-$PLX_VERSION-USC
+    zip -r PlexMediaServer-$PLX_VERSION-`uname -m`-USC.zip PlexMediaServer-$PLX_VERSION-USC
+	mv *.deb *.changes *.zip $PLX_OUTDIR
 	rm -rf debian-tmp
 fi
 
