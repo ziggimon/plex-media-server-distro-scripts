11c4fa4b2e37d8be1a75a1fae505651846549a25
Tobias Hieta <tobias@plexapp.com>
2012-05-31 10:21:34 +0200

    Updated ubuntu package scripts for USC

--- ubuntu/create_plex_pkg.sh
+++ ubuntu/create_plex_pkg.sh
@@ -4,20 +4,17 @@ if [[ $NODE_NAME == Linux-Ubuntu* ]];
 then
 	echo "Building Ubuntu package"
 	rm -f *.deb *.changes
-	mkdir PlexMediaServer-$PLX_VERSION-USC
-	cd PlexMediaServer-$PLX_VERSION-USC
-    cp ../plexmediamanager.* .
+	mkdir PlexMediaServer-$PLX_VERSION
+	cd PlexMediaServer-$PLX_VERSION
+  cp ../plexmediamanager.* .
 	ln -s $PLX_SRCDIR src
 	cp -r ../debian .
 	export EMAIL="jenkins@plexapp.com"
 	export NAME="Plex CI Team"
 	dch -b -v $PLX_VERSION "Automatic build $BUILD_NUMBER"
-    cd ..
-    zip -r PlexMediaServer-$PLX_VERSION-`uname -m`-USC.zip PlexMediaServer-$PLX_VERSION-USC
-    cd PlexMediaServer-$PLX_VERSION-USC
 	fakeroot dpkg-buildpackage -us -uc -b
 	cd ..
 	mv *.deb *.changes *.zip $PLX_OUTDIR
-	rm -rf PlexMediaServer-$PLX_VERSION-USC
+	rm -rf PlexMediaServer-$PLX_VERSION
 fi
 
---
+++ ubuntu_usc/create_plex_pkg.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ $NODE_NAME == Linux-Ubuntu* ]];
+then
+  ARCH=`dpkg-architecture -qDEB_BUILD_ARCH`
+  DIRNAME=PlexMediaServer-$PLX_VERSION-$ARCH-USC
+	echo "Building Ubuntu USC package"
+	rm -f *.deb *.changes
+	mkdir $DIRNAME
+	cd $DIRNAME
+  ln -s $PLX_SRCDIR $ARCH
+  cp -r ../debian ../icons .
+  cd ..
+  zip -r $PLX_OUTDIR/$DIRNAME.zip $DIRNAME
+  rm -rf $DIRNAME
+fi
+
