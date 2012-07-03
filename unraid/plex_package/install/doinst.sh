b1793afe3427ed44f31a3b06a8b754dd4303c342
Dirk Spittler <git@spidisoft.de>
2012-07-03 07:25:46 +0200

    set default settings via install script

--- unraid/plex_package/install/doinst.sh
+++ unraid/plex_package/install/doinst.sh
@@ -6,22 +6,48 @@ if  [ $? -ne 0 ]; then
         cp /etc/passwd /etc/shadow /boot/config
 fi
 
-#first install copy default settings 
-test -e /boot/config/plugins/plexmediaserver/plex_settings.cfg || cp /boot/config/plugins/plexmediaserver/plex_default.cfg /boot/config/plugins/plexmediaserver/plex_settings.cfg
-
 #source /boot/config/plugins/plexmediaserver/plex_settings.cfg
+CONFIG="/boot/config/plugins/plexmediaserver/settings.cfg"
 
-# create Library and change owner if it doens't exist 
-#if [ ! -d $DEFAULT_PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR ]; then
-#	echo "Create Plex Media Server Library directory"
-#	mkdir -p "$DEFAULT_PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR/Application Support/"
-#	chown -R $DEFAULT_RUNAS:users $DEFAULT_PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR
-#else
-#if it exist check owner
-#	if [ "$(stat -c %U $DEFAULT_PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR)"  != "$DEFAULT_RUNAS" ]; then
-#	echo "Change owner of existing Plex Media Server Library directory"
-#	chown -R $DEFAULT_RUNAS:users $DEFAULT_PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR
-#	fi 
-#fi
+if [ ! -f $CONFIG_FILE ]; then
+	
+	echo "# Plex Media Server Config file." >> $CONFIG_FILE
+	echo "#Set autostart with array" >> $CONFIG_FILE
+	echo "ENABLED=\"false\"" >> $CONFIG_FILE
+	
+	echo "# Set home of Plex Media Server" >> $CONFIG_FILE 
+	echo "PLEX_MEDIA_SERVER_HOME=\"/usr/local/plexmediaserver\"" >> $CONFIG_FILE
+	
+	echo "# Set home for Plex metadata" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=\"/mnt/cache/appdata/plex/Library/Application Support\"" >> $CONFIG_FILE
+	
+	echo "# the number of plugins that can run at the same time" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6" >> $CONFIG_FILE
+	
+	echo "# ulimit -s $PLEX_MEDIA_SERVER_MAX_STACK_SIZE" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_MAX_STACK_SIZE=10000" >> $CONFIG_FILE
+	
+	echo "# ulimit -l $PLEX_MEDIA_SERVER_MAX_LOCK_MEM" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_MAX_LOCK_MEM=3000" >> $CONFIG_FILE
+	
+	echo "# ulimit -n $PLEX_MEDIA_SERVER_MAX_OPEN_FILES" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_MAX_OPEN_FILES=4096" >> $CONFIG_FILE
+	
+	echo "# where the mediaserver should store the transcodes" >> $CONFIG_FILE
+	echo "PLEX_MEDIA_SERVER_TMPDIR=/tmp" >> $CONFIG_FILE
+	
+	echo "export LD_LIBRARY_PATH=\"${PLEX_MEDIA_SERVER_HOME}\"" >> $CONFIG_FILE
+	echo "export TMPDIR=\"${PLEX_MEDIA_SERVER_TMPDIR}\"" >> $CONFIG_FILE
+	echo "export PLEX_MEDIA_SERVER_HOME=$PLEX_MEDIA_SERVER_HOME" >> $CONFIG_FILE
+	echo "export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=$PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR" >> $CONFIG_FILE
+	echo "export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=$PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS" >> $CONFIG_FILE
+	echo "export PLEX_MEDIA_SERVER_TMPDIR=$PLEX_MEDIA_SERVER_TMPDIR" >> $CONFIG_FILE
+	echo "export LD_LIBRARY_PATH=$PLEX_MEDIA_SERVER_HOME" >> $CONFIG_FILE
+	echo "export LC_ALL=\"en_US.UTF-8\"" >> $CONFIG_FILE
+	echo "export LANG=\"en_US.UTF-8\"" >> $CONFIG_FILE
+	echo "ulimit -l $PLEX_MEDIA_SERVER_MAX_LOCK_MEM" >> $CONFIG_FILE
+	echo "ulimit -s $PLEX_MEDIA_SERVER_MAX_STACK_SIZE" >> $CONFIG_FILE
+	echo "ulimit -n $PLEX_MEDIA_SERVER_MAX_OPEN_FILES" >> $CONFIG_FILE
+fi
 
 
