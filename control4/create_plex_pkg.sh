90bc29e7844be232682d4808f33624650931a334
Tobias Hieta <tobias@plexapp.com>
2012-10-03 13:01:40 +0000

    Whoops -x is in the muscle memory

--- control4/create_plex_pkg.sh
+++ control4/create_plex_pkg.sh
@@ -5,6 +5,6 @@ then
   DIRNAME=PlexMediaServer-$PLX_VERSION-control4-arm
 	echo "Building Control4 package"
   ln -s $PLX_SRCDIR $DIRNAME
-  tar -cxjf $PLX_OUTDIR/$DIRNAME.tar.bz2 $DIRNAME
+  tar -cjf $PLX_OUTDIR/$DIRNAME.tar.bz2 $DIRNAME
 fi
 
