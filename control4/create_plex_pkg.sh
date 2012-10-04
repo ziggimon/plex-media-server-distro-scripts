2896bdc26129670f380e045d5004cd6080e8d5e9
Tobias Hieta <tobias@plexapp.com>
2012-10-04 06:01:59 +0000

    Really fix the control4 package script

--- control4/create_plex_pkg.sh
+++ control4/create_plex_pkg.sh
@@ -2,9 +2,9 @@
 
 if [[ $label == build-linux-control4* ]];
 then
-  DIRNAME=PlexMediaServer-$PLX_VERSION-control4-arm
+  DIRNAME=PlexMediaServer-$PLEX_VERSION-control4-arm
 	echo "Building Control4 package"
-  ln -s $PLX_SRCDIR $DIRNAME
-  tar -cjf $PLX_OUTDIR/$DIRNAME.tar.bz2 $DIRNAME
+  ln -s $PLEX_SRCDIR $DIRNAME
+  tar -cjf $PLEX_OUTDIR/$DIRNAME.tar.bz2 $DIRNAME
 fi
 
