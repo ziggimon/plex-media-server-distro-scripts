cfc7da0c3fb6084773990ee3052d0e126a76afa9
Tobias Hieta <tobias@hieta.se>
2013-02-13 13:44:51 +0000

    ReadyNAS6 split package into smaller parts

--- readynas6/create_plex_pkg.sh
+++ readynas6/create_plex_pkg.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-DIRECTORY=PlexMediaServer-$PLEX_VERSION/apps/plexmediaserver
+DIRECTORY_META=PlexMediaServer-$PLEX_VERSION/meta/apps/plexmediaserver
+DIRECTORY_BIN=PlexMediaServer-$PLEX_VERSION/bin/apps/plexmediaserver
 
 if [ $PLEX_CONFIG == ubuntu-x86_64 ];
 then
 	echo "Building ReadyNAS 6 package"
 	rm -f *.deb *.changes
-	mkdir -p $DIRECTORY
-    cp -r readynas-files/* $DIRECTORY
+	mkdir -p $DIRECTORY_META
+	mkdir -p $DIRECTORY_BIN
+    cp -r readynas-files/* $DIRECTORY_META
     cp -r debian PlexMediaServer-$PLEX_VERSION
-	cp -r $PLEX_SRCDIR $DIRECTORY/Binaries
-    cat readynas-files/config.xml | sed s/##VERSION##/$PLEX_VERSION/g > $DIRECTORY/config.xml
+	cp -r $PLEX_SRCDIR $DIRECTORY_BIN/Binaries
+    cat readynas-files/config.xml | sed s/##VERSION##/$PLEX_VERSION/g > $DIRECTORY_META/config.xml
     cd PlexMediaServer-$PLEX_VERSION
 	export EMAIL="jenkins@plexapp.com"
 	export NAME="Plex CI Team"
--- readynas6/readynas-files/start.sh
+++ readynas6/readynas-files/start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ ! -e /apps/plexmediaserver/Binaries/Plex\ Media\ Server ]
+then
+    apt-get update
+    apt-get -y install plexmediaserver-ros6-binaries
+    #dpkg -i /tmp/plexmediaserver-ros6-binaries_0.9.7.13.0-ad8288a_amd64.deb
+fi
+
 . /apps/plexmediaserver/plexmediaserver_environment
 /apps/plexmediaserver/Binaries/Plex\ Media\ Server &
 
