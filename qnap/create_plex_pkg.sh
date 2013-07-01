004f4dc72cd6d2e3a63a9e23022155d9357773c8
Thomas Kupper <thomas.kupper@gmail.com>
2013-07-01 21:24:23 +0200

    Add arm-x19 platform handling

--- qnap/create_plex_pkg.sh
+++ qnap/create_plex_pkg.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-if [[ $PLEX_CONFIG == debian-i686 ]];
+if [[ $PLEX_CONFIG == debian-i686 || $PLEX_CONFIG == synology-arm ]];
 then
   echo "build QNAP package"
   PLEX_SHORT_VERSION=${PLEX_VERSION:0:10}
   cat qpkg.cfg.in |sed "s|@PLEX_VERSION@|$PLEX_SHORT_VERSION|g;s|@PLEX_SRCDIR@|$PLEX_SRCDIR|g" > qnap-files/qpkg.cfg
   pushd qnap-files
-  /QNAP/QDK_2.2/bin/qbuild -q --build-arch x86
+  if [[ $PLEX_CONFIG == debian-i686 ]];
+  then
+    outarch=x86
+  else
+    outarch=arm-x19
+  fi
+  /QNAP/QDK_2.2/bin/qbuild -q --build-arch $outarch
   popd
-  mv qnap-files/build/PlexMediaServer_${PLEX_SHORT_VERSION}_x86.qpkg $PLEX_OUTDIR/PlexMediaServer_${PLEX_VERSION}_x86.qpkg
+  mv qnap-files/build/PlexMediaServer_${PLEX_SHORT_VERSION}_${outarch}.qpkg $PLEX_OUTDIR/PlexMediaServer_${PLEX_VERSION}_${outarch}.qpkg
 fi
