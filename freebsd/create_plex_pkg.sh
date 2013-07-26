e18ea1f097e3b7a6926400b8c634b9e55e9a2a63
Mark Walker <ziggimon@raindogs.dk>
2013-07-26 09:49:48 +0200

    Adding initial FreeBSD packaging.
    This should get a tar.bz2 package out. Me thinks.

---
+++ freebsd/create_plex_pkg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $PLEX_CONFIG == freebsd-x86_64 ]; then
+    echo "build BSD tar.bz2"
+    rm -rf freebsd_temp
+    mkdir freebsd_temp
+    cd _freebsd_temp
+    RELEASE=`echo ${PLEX_VERSION}| cut -d"-" -f1`
+    GIT_VERSION=`echo ${PLEX_VERSION} | cut -d"-" -f2`
+    mkdir -p PlexMediaServer-${RELEASE}-${GIT_VERSION}
+    cp -r ${PLEX_SRCDIR}/* PlexMediaServer-${RELEASE}-${GIT_VERSION}
+    tar cjf PlexMediaServer-${RELEASE}-${GIT_VERSION}.tar.bz2 PlexMediaServer-${RELEASE}-${GIT_VERSION}
+    mv PlexMediaServer-${RELEASE}-${GIT_VERSION}.tar.bz2 $PLEX_OUTDIR}/.
+fi
