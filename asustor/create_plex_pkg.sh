815e5a7d7bef7fef60e1c84e9493ff9144fec9eb
Tobias Hieta <tobias@plexapp.com>
2013-08-16 11:36:59 +0200

    Updates for ADM 2.0

--- asustor/create_plex_pkg.sh
+++ asustor/create_plex_pkg.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-if [ $PLEX_CONFIG == ubuntu-x86_64 ];
+if [ $PLEX_CONFIG == ubuntu-i686 -o $PLEX_CONFIG == ubuntu-x86_64 ];
 then
 	echo "Building ASUStor package"
 	if [ -d build ]; then
 		rm -rf build
 	fi
 	mkdir build
+  
+  if [ $PLEX_CONFIG == "ubuntu-i686" ]; then
+    ARCH="i386"
+  else
+    ARCH="x86_64"
+  fi
 	
 	cp -r CONTROL build/
 	rm build/CONTROL/config.json.in
-	cat CONTROL/config.json.in | sed "s/#VERSION#/$PLEX_VERSION/g" > build/CONTROL/config.json
+	cat CONTROL/config.json.in | sed "s/#VERSION#/$PLEX_VERSION/g" | sed "s/#ARCH#/$ARCH/g" > build/CONTROL/config.json
 	mkdir build/plexmediaserver
 	cp -r $PLEX_SRCDIR/* build/plexmediaserver
-	bin/apkg_build build $PLEX_OUTDIR
+	bin/apkg-tools.py create --destination $PLEX_OUTDIR build
 	rm -rf build
 fi
 
